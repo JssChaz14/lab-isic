292c90d36d0e6b539b1539113cc89df43da96407
Edgar Chávez <ecd.teckay@gmail.com>
2023-06-27 17:53:41 -0600

    error in budget a-json

--- models/models/reserve-laboratory.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ReserveLaboratory } from '../../src/app/models/reserve-laboratory';
-
-describe('ReserveLaboratory', () => {
-  it('should create an instance', () => {
-    expect(new ReserveLaboratory()).toBeTruthy();
-  });
-});
